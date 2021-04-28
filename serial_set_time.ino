0a536edfa5ee5154c48c1f2dcd1acff3702db49f
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2021-04-28 09:07:01 +0200

    Update serial_set_time.ino

--- serial_set_time.ino
+++ serial_set_time.ino
@@ -10,14 +10,12 @@
    compare time with another clock to know the seconds to adjust per day
    then set sectoadjustperday = +8.5; // + or - your ?? seconds to adjust per day
    secondsperday is divided by sectoadjustperday and a second + or - is adjusted
-
 */
 #include <stdio.h>
 #include <TimeLib.h>
 #include <TimeAlarms.h>  // https://github.com/PaulStoffregen/TimeAlarms
 AlarmId id;
-#define TIME_HEADER  "T"   // Header tag for serial time sync message
-#define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
+
 long adjusttime;
 
 
@@ -39,8 +37,7 @@ void setup()  {
   Serial.begin(9600);
   while (!Serial) ; // Needed for Leonardo only
   pinMode(13, OUTPUT);
-  setSyncProvider( requestSync);  //set function to call when sync required
-  Serial.println("Waiting for sync message");
+ 
   setTime(8, 38, 00, 24, 4, 21);
   set_clock();
   // setDateTime(__DATE__,__TIME__);
@@ -106,25 +103,8 @@ void printDigits(int digits) {
 }
 
 
-void processSyncMessage() {
-  unsigned long pctime;
-  const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013
 
-  if (Serial.find(TIME_HEADER)) {
-    pctime = Serial.parseInt();
-    if ( pctime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
-      setTime(pctime); // Sync Arduino clock to the time received on the serial port
-    }
 
-  }
-  setTime(8, 04, 00, day(), month(), year());
-}
-
-time_t requestSync()
-{
-  Serial.write(TIME_REQUEST);
-  return 0; // the time will be sent later in response to serial mesg
-}
 
 void Repeats_adjusttime() {
   if (sectoadjustperday < 0) {
